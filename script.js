f3150a17d8792ce7c2ee78e31d21b1b45c5b87d4
kerry <kerry@warnes.com>
2022-10-07 14:17:36 +0100

    .

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const ball = document.querySelectorAll('.ball');
+// enables the submit button once a ball rating has been selected
+var iv = 1;
+function ballStyle(iv) {
+  var ballRate = document.querySelectorAll(".ball-container .ball");
 
-for (let element of ball) {
-    element.addEventListener('click', () => {
-        element.classList.toggle('ball-placeholder-a')
-    })
+  for (var i = 0; i < ballRate.length; i++) {
+      ballRate[i].classList.remove("ballActive");
+
+  }
+  ballRate[iv-1].classList.add("ballActive");
+
+  var remove = document.querySelector(".submitBtn").removeAttribute("disabled");
+console.log(remove);
 }
 
-
-const allNumbers = document.querySelectorAll(".ball")
+//Takes the ball rating and adds it to the thank you page span text
+const allNumbers = document.querySelectorAll(".ball");
 allNumbers.forEach((ball) => {
   ball.addEventListener('click', () => {
       rateNum.innerHTML = ball.innerHTML;
   })
 })
 
-function submit(){
-    document.getElementById("start-panel").style.visibility = "hidden";
-    document.getElementById("thanks-panel").style.visibility = "visible";
-
+//hides the start-panel and then shows the thanks-panel
+function submitGo(){
+  document.getElementById("start-panel").style.visibility = "hidden";
+  document.getElementById("thanks-panel").style.visibility = "visible";
 }
